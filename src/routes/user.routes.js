52a934d5b23e7abcdc04f871a17a30ed622aaeca
sajad thapa <sajadthapa04@gmail.com>
2025-02-14 10:49:29 +0545

    made some fomatting on user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logOutUser,refreshAccessToken, changeUserCurrentPassword, getCurrentUser, updateCurrentUser, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logOutUser,
+    refreshAccessToken,
+    changeUserCurrentPassword,
+    getCurrentUser,
+    updateCurrentUser,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchhistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
 
@@ -21,11 +33,13 @@ router.route("/login").post(loginUser)
 //secured route
 router.route("/logout").post(verifyJwt, logOutUser)
 router.route("/refreshToken").post(refreshAccessToken)
-router.route("/changePassword").post(verifyJwt,changeUserCurrentPassword);
-router.route("/currentUser").post(verifyJwt,getCurrentUser);
-router.route("/updateUser").post(verifyJwt,updateCurrentUser)
-router.route("/updateAvatar").post(verifyJwt,upload.single("avatar"),updateUserAvatar)
-router.route("/updateCoverImage").post(verifyJwt,upload.single("coverImage"),updateUserCoverImage);
+router.route("/changePassword").post(verifyJwt, changeUserCurrentPassword);
+router.route("/currentUser").post(verifyJwt, getCurrentUser);
+router.route("/updateUser").patch(verifyJwt, updateCurrentUser)
+router.route("/updateAvatar").post(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/updateCoverImage").post(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/watch_history").get(verifyJwt, getWatchhistory);
 
 
 export default router;
